c13e3fbdacb942b56b0d03a44162df291df54ed6
Spartak Maznytskyi <spartakm@ukr.net>
2022-12-10 21:15:23 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup();
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" />
+              </a>`;
+    })
+    .join("");
+}
