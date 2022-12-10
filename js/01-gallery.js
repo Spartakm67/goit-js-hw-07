969df7d0d782a1e323362aa897aa9a48cd9d5234
Spartak Maznytskyi <spartakm@ukr.net>
2022-12-10 17:10:22 +0200

    Markup rendered

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup();
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
